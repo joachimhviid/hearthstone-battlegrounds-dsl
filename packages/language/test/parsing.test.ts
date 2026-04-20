e07ef1248df96ce0632437fdfc6b3e3f9f7dbb4f
joachimhviid <joac1099@gmail.com>
2026-04-20 14:41:31 +0200

    test: parse valid minion token

--- packages/language/test/parsing.test.ts
+++ packages/language/test/parsing.test.ts
@@ -1,7 +1,7 @@
 import { beforeAll, describe, expect, test } from "vitest";
 import { EmptyFileSystem, type LangiumDocument } from "langium";
 import { parseHelper } from "langium/test";
-import type { Model } from "hearthstone-battlegrounds-dsl-language";
+import type { Action, Model, SummonAction } from "hearthstone-battlegrounds-dsl-language";
 import { createHearthstoneBattlegroundsDslServices, isModel } from "hearthstone-battlegrounds-dsl-language";
 
 let services: ReturnType<typeof createHearthstoneBattlegroundsDslServices>;
@@ -20,13 +20,12 @@ describe('Parsing tests', () => {
 
     test('parse minion with numeric tier', async () => {
         document = await parse(`
-                        Beast
-
             Minion AlleyCat {
-              name "Alley Cat"
-              tier 1
-              attack 1
-              health 1
+                name "Alley Cat"
+                tier 1
+                attack 1
+                health 1
+                tribes Beast
             }
         `);
 
@@ -36,9 +35,48 @@ describe('Parsing tests', () => {
             minionName: 'Alley Cat',
             tier: 1,
             attack: 1,
-            health: 1
+            health: 1,
+            tribes: ['Beast'],
         });
     });
+
+    test('parse summon is valid token', async () => {
+        document = await parse(`
+            Minion AlleyCat {
+                name "Alley Cat"
+                tier 1
+                attack 1
+                health 1
+                tribes Beast
+                Battlecry summon TabbyCat
+            }
+
+            Minion token TabbyCat {
+                name "Tabby Cat"
+                tier 1
+                attack 1
+                health 1
+                tribes Beast
+            }
+        `);
+
+        expect(checkDocumentValid(document)).toBeUndefined();
+        expect(document.parseResult.value.minions[1]).toMatchObject({
+            name: 'TabbyCat',
+            minionName: 'Tabby Cat',
+            token: true,
+        })
+        document.parseResult.value.minions[0].effects.map((effect) => {
+            effect.actions.map((action) => {
+                expect(action.$type).toBe('SummonAction')
+                if (actionIsSummonAction(action)) {
+                    expect(action.minion.error).toBeUndefined()
+                    expect(action.minion.ref?.name).toBe('TabbyCat')
+                    expect(action.minion.ref?.token).toBe(true)
+                }
+            })
+        })
+    })
 });
 
 function checkDocumentValid(document: LangiumDocument): string | undefined {
@@ -46,4 +84,8 @@ function checkDocumentValid(document: LangiumDocument): string | undefined {
         || document.parseResult.value === undefined && `ParseResult is 'undefined'.`
         || !isModel(document.parseResult.value) && `Root AST object is a ${document.parseResult.value.$type}, expected a 'Model'.`
         || undefined;
+}
+
+function actionIsSummonAction(action: Action): action is SummonAction {
+    return action.$type === 'SummonAction'
 }
